6ce6dce1134f6bf0699f348ad42e6226af236720
ZsoltKovari <kovari.zsolt91@gmail.com>
2016-02-12 18:15:09 +0100

    Delete query metrics

--- eu.mondo.map.core/src/main/java/eu/mondo/map/core/metrics/queries/ConstantsMetric.java
+++
@@ -1,9 +0,0 @@
-package eu.mondo.map.core.metrics.queries;
-
-public class ConstantsMetric extends QueryMetric {
-
-//	@Override
-//	protected String getIdentifier() {
-//		return "Constants";
-//	}
-}
