324e1f63cf0f730c9e4ad969107e16466f0eba1e
ZsoltKovari <kovari.zsolt91@gmail.com>
2016-02-19 22:37:04 +0100

    Well, add again

---
+++ eu.mondo.map.modelmetrics/src/test/java/eu/mondo/map/modelmetrics/tests/PairwiseMultiplexityTest.java
@@ -0,0 +1,141 @@
+package eu.mondo.map.modelmetrics.tests;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import eu.mondo.map.core.graph.Network;
+import eu.mondo.map.modelmetrics.scalar.typed.PairwiseMultiplexity;
+
+public class PairwiseMultiplexityTest {
+
+	protected PairwiseMultiplexity pairwiseMultiplexity;
+	protected Network<String> network;
+	protected String dim1;
+	protected String dim2;
+	protected String dim3;
+	protected String node1;
+	protected String node2;
+	protected String node3;
+	protected String node4;
+	protected String node5;
+	protected String node6;
+
+	@Before
+	public void init() {
+		network = new Network<String>();
+		pairwiseMultiplexity = new PairwiseMultiplexity();
+		node1 = "node1";
+		node2 = "node2";
+		node3 = "node3";
+		node4 = "node4";
+		node5 = "node5";
+		node6 = "node6";
+		dim1 = "dim1";
+		dim2 = "dim2";
+		dim3 = "dim3";
+	}
+
+	public void checkMutiplexity(String dim1, String dim2, double expected) {
+		Assert.assertEquals(expected, pairwiseMultiplexity.getValues().get(dim1 + "-" + dim2)
+				.doubleValue(), 0.01);
+	}
+
+	private void checkMutiplexityExclusive(String dim1, String dim2, double expected) {
+		Assert.assertEquals(expected,
+				pairwiseMultiplexity.getValues().get(dim1 + "-" + dim2 + "-Exclusive")
+						.doubleValue(), 0.01);
+
+	}
+
+	private void calculateMultiplexity() {
+		pairwiseMultiplexity.calculate(network, dim1, dim2);
+		pairwiseMultiplexity.calculate(network, dim1, dim3);
+		pairwiseMultiplexity.calculate(network, dim2, dim3);
+		pairwiseMultiplexity.calculateExclusive(network, dim1, dim2);
+		pairwiseMultiplexity.calculateExclusive(network, dim1, dim3);
+		pairwiseMultiplexity.calculateExclusive(network, dim2, dim3);
+	}
+
+	@Test
+	public void zeroIntersection1() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim2, node3, node4);
+		network.addEdge(dim3, node5, node6);
+
+		calculateMultiplexity();
+		checkMutiplexity(dim1, dim2, 0.0);
+		checkMutiplexity(dim1, dim3, 0.0);
+		checkMutiplexity(dim2, dim3, 0.0);
+		checkMutiplexityExclusive(dim1, dim2, 0.0);
+		checkMutiplexityExclusive(dim1, dim3, 0.0);
+		checkMutiplexityExclusive(dim2, dim3, 0.0);
+	}
+
+	@Test
+	public void intersection1() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim2, node3, node4);
+		network.addEdge(dim3, node1, node5);
+
+		calculateMultiplexity();
+
+		checkMutiplexity(dim1, dim2, 0.0);
+		checkMutiplexity(dim1, dim3, 0.2);
+		checkMutiplexity(dim2, dim3, 0.0);
+		checkMutiplexityExclusive(dim1, dim2, 0.0);
+		checkMutiplexityExclusive(dim1, dim3, 0.33);
+		checkMutiplexityExclusive(dim2, dim3, 0.0);
+	}
+
+	@Test
+	public void intersection2() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim2, node3, node4);
+		network.addEdge(dim3, node3, node2);
+
+		calculateMultiplexity();
+		checkMutiplexity(dim1, dim2, 0.0);
+		checkMutiplexity(dim1, dim3, 0.25);
+		checkMutiplexity(dim2, dim3, 0.25);
+		checkMutiplexityExclusive(dim1, dim2, 0.0);
+		checkMutiplexityExclusive(dim1, dim3, 0.333);
+		checkMutiplexityExclusive(dim2, dim3, 0.333);
+	}
+
+	@Test
+	public void intersection3() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node3, node4);
+		network.addEdge(dim2, node1, node3);
+		network.addEdge(dim3, node2, node5);
+		network.addEdge(dim3, node1, node6);
+
+		calculateMultiplexity();
+		checkMutiplexity(dim1, dim2, 0.33);
+		checkMutiplexity(dim1, dim3, 0.33);
+		checkMutiplexity(dim2, dim3, 0.166);
+		checkMutiplexityExclusive(dim1, dim2, 0.5);
+		checkMutiplexityExclusive(dim1, dim3, 0.33);
+		checkMutiplexityExclusive(dim2, dim3, 0.2);
+	}
+
+	@Test
+	public void intersection4() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node1, node3);
+		network.addEdge(dim1, node2, node3);
+		network.addEdge(dim2, node2, node3);
+		network.addEdge(dim3, node1, node4);
+		network.addEdge(dim3, node2, node4);
+		network.addEdge(dim3, node2, node5);
+
+		calculateMultiplexity();
+		checkMutiplexity(dim1, dim2, 0.40);
+		checkMutiplexity(dim1, dim3, 0.40);
+		checkMutiplexity(dim2, dim3, 0.20);
+		checkMutiplexityExclusive(dim1, dim2, 0.666);
+		checkMutiplexityExclusive(dim1, dim3, 0.4);
+		checkMutiplexityExclusive(dim2, dim3, 0.2);
+	}
+}
