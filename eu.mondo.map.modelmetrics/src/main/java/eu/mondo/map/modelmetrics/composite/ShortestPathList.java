459247c18cb36e8fd25a6b4ace7612fcbd1a7152
ZsoltKovari <kovari.zsolt91@gmail.com>
2016-02-24 18:17:44 +0100

    Implement shortest path metric, add some tests

--- eu.mondo.map.modelmetrics/src/main/java/eu/mondo/map/modelmetrics/composite/ShortestPathList.java
+++ eu.mondo.map.modelmetrics/src/main/java/eu/mondo/map/modelmetrics/composite/ShortestPathList.java
@@ -1,67 +1,174 @@
 package eu.mondo.map.modelmetrics.composite;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 
-public class ShortestPathList {
+import eu.mondo.map.core.graph.Node;
+import eu.mondo.map.core.metrics.ListMetric;
 
-//	private List<Integer> shortestPaths;
-//
-//	private int maxDepth;
-//
-//	private int pairs;
-//
-//	public ShortestPathList() {
-//		shortestPaths = new ArrayList<Integer>();
-//		maxDepth = 50;
-//		pairs = 100;
-//	}
-//
-////	@Override
-//	public void calculate() {
-////		if (shortestPaths.size() == 0) {
-////			metricValue = 0.0;
-////			return;
-////		}
-////		double sum = 0.0;
-////		for (Integer distance : shortestPaths) {
-////			sum += distance;
-////		}
-////		metricValue = sum / shortestPaths.size();
-//
-//	}
-//
-//	public boolean add(Integer e) {
-//		return shortestPaths.add(e);
-//	}
-//
-//	public void clear() {
-//		shortestPaths.clear();
-////		metricValue = 0;
-//	}
-//
-////	@Override
-////	protected String getIdentifier() {
-////		return "AvgShortestPath";
-////	}
-//
-//	public int getMaxDepth() {
-//		return maxDepth;
-//	}
-//
-//	public void setMaxDepth(int maxDepth) {
-//		this.maxDepth = maxDepth;
-//	}
-//
-//	public int getPairs() {
-//		return pairs;
-//	}
-//
-//	public void setPairs(int pairs) {
-//		this.pairs = pairs;
-//	}
-//
-//	@Override
-//	public String getName() {
-//		return "ShortestPathList";
-//	}
+public class ShortestPathList extends ListMetric<Integer> {
 
+	public ShortestPathList() {
+		super("ShortestPathList");
+	}
+
+	protected Map<Node<?>, Node<?>> visits;
+	protected Map<Node<?>, Integer> depths;
+	protected Map<Node<?>, Integer> results;
+
+	public List<Path> calculate(final Node<?> sourceNode, final Node<?> targetNode) {
+		if (sourceNode == null) {
+			throw new IllegalArgumentException("The sourceNode is null!");
+		}
+		if (targetNode == null) {
+			throw new IllegalArgumentException("The targetNode is null!");
+		}
+		init();
+
+		Queue<Node<?>> queue = new LinkedList<Node<?>>();
+		queue.add(sourceNode);
+		visits.put(sourceNode, null);
+		depths.put(sourceNode, 0);
+
+		Node<?> currentNode;
+		int depth = 0;
+		while (!queue.isEmpty()) {
+			currentNode = queue.remove();
+			if (higherDepth(currentNode, depth)) {
+				break;
+			}
+			for (Node<?> neighborNode : currentNode.getOutgoingNeighbors().keySet()) {
+				if (neighborNode == targetNode) {
+					results.put(currentNode, 0);
+				}
+				if (!visits.containsKey(neighborNode)) {
+					visit(neighborNode, currentNode);
+					depth = depths.get(neighborNode);
+					queue.add(neighborNode);
+				}
+			}
+		}
+		List<Path> paths;
+		if (!results.isEmpty()) {
+			paths = resolveResults(targetNode);
+		} else {
+			paths = new ArrayList<Path>();
+		}
+
+		clearFields();
+		return paths;
+	}
+
+	protected List<Path> resolveResults(final Node<?> targetNode) {
+		int depth = depths.get(targetNode);
+		List<Path> paths = new ArrayList<Path>();
+		for (Node<?> node : results.keySet()) {
+			Path path = new Path(depth);
+			path.add(targetNode);
+			path.add(node);
+			Node<?> parent;
+			while ((parent = visits.get(node)) != null) {
+				path.add(parent);
+				node = parent;
+			}
+			paths.add(path);
+		}
+		return paths;
+	}
+
+	/**
+	 * 
+	 * @param currentNode
+	 * @param expectedDepth
+	 * 
+	 * @return true if the results Map is not empty and the currentNode's depth is higher or equal than
+	 *         the expectedDepth, false otherwise
+	 */
+	private boolean higherDepth(final Node<?> currentNode, final int expectedDepth) {
+		if (!results.isEmpty()) {
+			return depths.get(currentNode) >= expectedDepth;
+		}
+		return false;
+
+	}
+
+	protected void init() {
+		if (visits == null) {
+			visits = new HashMap<Node<?>, Node<?>>();
+		}
+		if (depths == null) {
+			depths = new HashMap<Node<?>, Integer>();
+		}
+		if (results == null) {
+			results = new HashMap<Node<?>, Integer>();
+		}
+		clearFields();
+	}
+
+	protected void clearFields() {
+		visits.clear();
+		depths.clear();
+		results.clear();
+	}
+
+	protected void visit(final Node<?> child, final Node<?> parent) {
+		visits.put(child, parent);
+		int depth = depths.get(parent);
+		depth++;
+		depths.put(child, depth);
+	}
+
+	public static class Path {
+
+		protected List<Node<?>> path;
+		protected int depth;
+
+		public Path() {
+			path = new ArrayList<Node<?>>();
+		}
+
+		public Path(int depth) {
+			path = new ArrayList<Node<?>>();
+			this.depth = depth;
+		}
+
+		public boolean isEmpty() {
+			return path.isEmpty();
+		}
+
+		public List<Node<?>> getPath() {
+			return path;
+		}
+
+		public int getDepth() {
+			return depth;
+		}
+
+		public void setDepth(int depth) {
+			this.depth = depth;
+		}
+
+		public int size() {
+			return path.size();
+		}
+
+		public boolean add(Node<?> e) {
+			return path.add(e);
+		}
+
+		public boolean addAll(Collection<? extends Node<?>> c) {
+			return path.addAll(c);
+		}
+
+		@Override
+		public String toString() {
+			return "Path [path=" + path + ", depth=" + depth + "]";
+		}
+
+	}
 }
---
+++ eu.mondo.map.modelmetrics/src/test/java/eu/mondo/map/modelmetrics/tests/ShortestPathListTest.java
@@ -0,0 +1,270 @@
+package eu.mondo.map.modelmetrics.tests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+
+import eu.mondo.map.core.graph.Node;
+import eu.mondo.map.modelmetrics.composite.ShortestPathList;
+import eu.mondo.map.modelmetrics.composite.ShortestPathList.Path;
+
+public class ShortestPathListTest extends ModelMetricTest {
+
+	protected ShortestPathList shortestPathList;
+
+	@Override
+	public void initMetric() {
+		shortestPathList = new ShortestPathList();
+	}
+
+	protected void checkDepth(int expected, List<Path> paths) {
+		for (Path path : paths) {
+			Assert.assertEquals(expected, path.getDepth());
+		}
+	}
+
+	protected void checkPath(Path path, List<String> vertices) {
+		List<Node<String>> nodes = new ArrayList<Node<String>>();
+		for (String vertex : vertices) {
+			nodes.add(network.getNode(vertex));
+		}
+
+		Assert.assertArrayEquals(nodes.toArray(), path.getPath().toArray());
+
+	}
+
+	protected void checkPathNumber(int expected, List<Path> paths) {
+		Assert.assertEquals(expected, paths.size());
+
+	}
+
+	protected void containsPath(List<Path> paths, ArrayList<String> vertices) {
+		List<Node<String>> nodes = new ArrayList<Node<String>>();
+		for (String vertex : vertices) {
+			nodes.add(network.getNode(vertex));
+		}
+		boolean contains = false;
+		for (Path p : paths) {
+			if (p.getPath().equals(nodes)) {
+				contains = true;
+			}
+		}
+		Assert.assertTrue(contains);
+
+	}
+
+	protected List<Path> calculate(String source, String target) {
+		Node<String> sourceNode = network.getNode(source);
+		Node<String> targetNode = network.getNode(target);
+
+		List<Path> paths = shortestPathList.calculate(sourceNode, targetNode);
+		return paths;
+	}
+
+	@Test
+	public void nullPath() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node2, node3);
+
+		List<Path> paths = calculate(node3, node2);
+		Assert.assertTrue(paths.isEmpty());
+	}
+
+	@Test
+	public void nullPath2() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node3, node4);
+		network.addEdge(dim1, node2, node4);
+
+		List<Path> paths = calculate(node1, node3);
+		Assert.assertTrue(paths.isEmpty());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void nullSource() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node2, node3);
+
+		calculate(node4, node2);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void nullTarget() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node2, node3);
+
+		calculate(node1, node4);
+	}
+
+	@Test
+	public void absentSource() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node2, node3);
+		network.addNode(node4);
+
+		List<Path> paths = calculate(node4, node3);
+		Assert.assertTrue(paths.isEmpty());
+	}
+
+	@Test
+	public void absentTarget() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node2, node3);
+		network.addNode(node4);
+
+		List<Path> paths = calculate(node1, node4);
+		Assert.assertTrue(paths.isEmpty());
+	}
+
+	@Test
+	public void singlePath1() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node2, node3);
+
+		List<Path> paths = calculate(node1, node3);
+		checkDepth(2, paths);
+		checkPathNumber(1, paths);
+		checkPath(paths.get(0), Lists.newArrayList(node3, node2, node1));
+
+	}
+
+	@Test
+	public void singlePath2() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node2, node3);
+
+		List<Path> paths = calculate(node2, node3);
+		checkDepth(1, paths);
+		checkPathNumber(1, paths);
+		checkPath(paths.get(0), Lists.newArrayList(node3, node2));
+	}
+
+	@Test
+	public void singlePath3() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node2, node3);
+		network.addEdge(dim1, node1, node3);
+
+		List<Path> paths = calculate(node1, node3);
+		checkDepth(1, paths);
+		checkPathNumber(1, paths);
+		checkPath(paths.get(0), Lists.newArrayList(node3, node1));
+	}
+
+	@Test
+	public void singlePath4() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node3, node2);
+		network.addEdge(dim1, node3, node1);
+		network.addEdge(dim1, node2, node1);
+		network.addEdge(dim1, node2, node3);
+
+		List<Path> paths = calculate(node1, node3);
+		checkDepth(2, paths);
+		checkPathNumber(1, paths);
+		checkPath(paths.get(0), Lists.newArrayList(node3, node2, node1));
+	}
+
+	@Test
+	public void singlePathMultiDimensional() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node2, node1);
+		network.addEdge(dim2, node2, node3);
+
+		List<Path> paths = calculate(node1, node3);
+		checkDepth(2, paths);
+		checkPathNumber(1, paths);
+		checkPath(paths.get(0), Lists.newArrayList(node3, node2, node1));
+	}
+
+	@Test
+	public void singlePathMultiDimensional2() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node2, node3);
+		network.addEdge(dim2, node2, node3);
+
+		List<Path> paths = calculate(node1, node3);
+		checkDepth(2, paths);
+		checkPathNumber(1, paths);
+		checkPath(paths.get(0), Lists.newArrayList(node3, node2, node1));
+	}
+
+	@Test
+	public void multiplePath2() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node1, node4);
+		network.addEdge(dim1, node2, node3);
+		network.addEdge(dim2, node2, node3);
+		network.addEdge(dim1, node4, node3);
+
+		List<Path> paths = calculate(node1, node3);
+		checkDepth(2, paths);
+		checkPathNumber(2, paths);
+		containsPath(paths, Lists.newArrayList(node3, node2, node1));
+		containsPath(paths, Lists.newArrayList(node3, node4, node1));
+	}
+
+	@Test
+	public void multiplePath3() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node1, node3);
+		network.addEdge(dim1, node2, node4);
+		network.addEdge(dim1, node3, node5);
+		network.addEdge(dim1, node4, node6);
+		network.addEdge(dim1, node5, node6);
+
+		List<Path> paths = calculate(node1, node6);
+		checkDepth(3, paths);
+		checkPathNumber(2, paths);
+		containsPath(paths, Lists.newArrayList(node6, node4, node2, node1));
+		containsPath(paths, Lists.newArrayList(node6, node5, node3, node1));
+	}
+
+	@Test
+	public void multiplePath4() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node1, node3);
+		network.addEdge(dim2, node1, node3);
+		network.addEdge(dim3, node1, node3);
+		network.addEdge(dim1, node2, node4);
+		network.addEdge(dim1, node3, node5);
+		network.addEdge(dim1, node4, node6);
+		network.addEdge(dim3, node4, node6);
+		network.addEdge(dim1, node5, node6);
+		network.addEdge(dim2, node5, node6);
+		network.addEdge(dim3, node5, node6);
+
+		List<Path> paths = calculate(node1, node6);
+		checkDepth(3, paths);
+		checkPathNumber(2, paths);
+		containsPath(paths, Lists.newArrayList(node6, node4, node2, node1));
+		containsPath(paths, Lists.newArrayList(node6, node5, node3, node1));
+	}
+
+	@Test
+	public void multiplePath5() {
+		network.addEdge(dim1, node1, node2);
+		network.addEdge(dim1, node1, node3);
+		network.addEdge(dim1, node2, node4);
+		network.addEdge(dim1, node2, node5);
+		network.addEdge(dim1, node3, node5);
+		network.addEdge(dim1, node4, node6);
+		network.addEdge(dim1, node4, node3);
+		network.addEdge(dim1, node5, node6);
+
+		List<Path> paths = calculate(node1, node6);
+		checkDepth(3, paths);
+		for (Path p : paths) {
+//			System.out.println(p);
+		}
+		checkPathNumber(3, paths);
+		containsPath(paths, Lists.newArrayList(node6, node4, node2, node1));
+		containsPath(paths, Lists.newArrayList(node6, node5, node3, node1));
+		containsPath(paths, Lists.newArrayList(node6, node5, node2, node1));
+	}
+
+}
