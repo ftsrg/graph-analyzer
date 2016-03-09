2bc6153b04ec558f953154b721105cd4323cd9f3
Gabor Szarnyas <szarnyasg@gmail.com>
2016-03-09 13:01:32 +0100

    Added TODO

--- eu.mondo.map.core/src/main/java/eu/mondo/map/core/metrics/Publishing.java
+++ eu.mondo.map.core/src/main/java/eu/mondo/map/core/metrics/Publishing.java
@@ -2,6 +2,7 @@ package eu.mondo.map.core.metrics;
 
 import java.util.List;
 
+// TODO this should be Publisher or Publishable ?
 public interface Publishing {
 
 	public List<PublishedMetric> resolve();
