2d84641667b4605486a1ab3c112d038cd66c0dd9
Gabor Szarnyas <szarnyasg@gmail.com>
2016-03-31 23:29:30 +0200

    RDFFactory working

--- eu.mondo.map.rdf/src/main/java/eu/mondo/map/rdf/RDFNetworkFactory.java
+++ eu.mondo.map.rdf/src/main/java/eu/mondo/map/rdf/RDFNetworkFactory.java
@@ -1,7 +1,12 @@
 package eu.mondo.map.rdf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openrdf.model.Resource;
 import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.RepositoryResult;
@@ -11,30 +16,39 @@ import eu.mondo.map.core.graph.Network;
 public class RDFNetworkFactory {
 
 	public static Network<Resource> createNetwork(final RepositoryConnection connection) throws RepositoryException {
-		Network<Resource> network = new Network<>();
+		final Network<Resource> network = new Network<>();
+		final List<Resource> resources = new ArrayList<>();
+		final RepositoryResult<Statement> statements = connection.getStatements(null, null, null, true);
 
-		RepositoryResult<Statement> statements = connection.getStatements(null, null, null, true);
-		
 		while (statements.hasNext()) {
-			System.out.println(statements.next());
+			final Statement statement = statements.next();
+
+			final Resource subject = statement.getSubject();
+			resources.add(subject);
+
+			final Value object = statement.getObject();
+			if (object instanceof Resource) {
+				resources.add((Resource) object);
+			}
 		}
 		
-//		while (objects.hasNext()) {
-//			obj = objects.next();
-//			network.addNode(obj);
-//			for (EReference ref : obj.eClass().getEAllReferences()) {
-//				if (!ref.isDerived()) {
-//					String referenceWithContainingClass = ref.getEContainingClass().getName() + "." + ref.getName();
-//					if (ref.isMany()) {
-//						for (EObject neighbor : (EList<EObject>) obj.eGet(ref, true)) {
-//							network.addEdge(referenceWithContainingClass, obj, neighbor);
-//						}
-//					} else {
-//						network.addEdge(referenceWithContainingClass, obj, (EObject) obj.eGet(ref, true));
-//					}
-//				}
-//			}
-//		}
+		for (Resource resource : resources) {
+			network.addNode(resource); 
+
+			RepositoryResult<Statement> outgoingEdges = connection.getStatements(resource, null, null, true);
+			while (outgoingEdges.hasNext()) {
+				final Statement edge = outgoingEdges.next();
+				final URI predicate = edge.getPredicate();
+				final Value object = edge.getObject();
+				
+				if (!(object instanceof Resource)) {
+					continue;
+				}
+				
+				network.addEdge(predicate.toString(), resource, (Resource) object);
+			}
+		}
+		
 		return network;
 	}
 }
