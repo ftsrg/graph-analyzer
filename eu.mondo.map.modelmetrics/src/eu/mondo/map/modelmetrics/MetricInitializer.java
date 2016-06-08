70526ab8c1511cbb265807170f646e33cfb38b9b
ZsoltKovari <kovari.zsolt91@gmail.com>
2016-06-08 21:09:47 +0200

    Add ModelMetrics enum and initializer interface

---
+++ eu.mondo.map.modelmetrics/src/eu/mondo/map/modelmetrics/MetricInitializer.java
@@ -0,0 +1,9 @@
+package eu.mondo.map.modelmetrics;
+
+import eu.mondo.map.core.metrics.Metric;
+
+public interface MetricInitializer {
+
+	public Metric instantiate();
+
+}
---
+++ eu.mondo.map.modelmetrics/src/eu/mondo/map/modelmetrics/ModelMetrics.java
@@ -0,0 +1,26 @@
+package eu.mondo.map.modelmetrics;
+
+import eu.mondo.map.modelmetrics.scalar.NumberOfEdges;
+import eu.mondo.map.modelmetrics.scalar.NumberOfNodes;
+
+public enum ModelMetrics implements MetricInitializer {
+
+	NumberOfEdges {
+
+		@Override
+		public NumberOfEdges instantiate() {
+			return new NumberOfEdges();
+		}
+
+	},
+	NumberOfNodes {
+
+		@Override
+		public NumberOfNodes instantiate() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	}
+
+}
