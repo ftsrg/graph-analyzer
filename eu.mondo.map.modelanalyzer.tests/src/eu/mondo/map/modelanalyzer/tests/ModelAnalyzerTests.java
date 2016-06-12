b3fa6f5f6271615f04dd4000b6e9638b7784fa5d
ZsoltKovari <kovari.zsolt91@gmail.com>
2016-06-12 13:15:57 +0200

    Create modelanalyzer tests, modify the corresponding api

--- eu.mondo.map.modelanalyzer.tests/src/eu/mondo/map/modelanalyzer/tests/ModelAnalyzerTests.java
+++ eu.mondo.map.modelanalyzer.tests/src/eu/mondo/map/modelanalyzer/tests/ModelAnalyzerTests.java
@@ -1,8 +1,20 @@
 package eu.mondo.map.modelanalyzer.tests;
 
+import static eu.mondo.map.modelmetrics.ModelMetrics.NumberOfEdges;
+import static eu.mondo.map.modelmetrics.ModelMetrics.NumberOfNodes;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import eu.mondo.map.core.metrics.Metric;
 import eu.mondo.map.core.metrics.SummaryMetric;
 import eu.mondo.map.modelanalyzer.ModelAnalyzer;
 import eu.mondo.map.modelmetrics.ModelMetrics;
@@ -12,24 +24,126 @@ public class ModelAnalyzerTests {
 
 	protected ModelAnalyzer analyzer;
 
+	public ModelAnalyzerTests() {
+		PropertyConfigurator.configure("src/log4j.properties");
+		Logger.getRootLogger().setLevel(Level.OFF);
+	}
+
 	@Before
 	public void init() {
 		analyzer = new ModelAnalyzer();
 	}
 
+	protected void checkSize(int expected) {
+		assertEquals(expected, analyzer.getMetrics().keySet().size());
+		assertEquals(expected, analyzer.getMetricsInOrder().size());
+	}
+
+	protected void checkMetric(ModelMetrics metric) {
+		Metric metricObject = analyzer.getMetric(metric);
+		Assert.assertNotNull(metricObject);
+		assertTrue(metric.instantiate().getClass().isInstance(metricObject));
+	}
+
 	@Test
 	public void testSummaryMetrics() {
 		analyzer.useSummary(new SummaryMetric<Integer, DegreeList<?>>());
+		fail("Not tested yet");
+	}
+
+	@Test
+	public void testAggregatedMetrics() {
+		// analyzer.useSummary(new SummaryMetric<Integer, DegreeList<?>>());
+		fail("Not tested yet");
 	}
 
 	@Test
 	public void testUse() {
-		analyzer.use(ModelMetrics.NumberOfEdges);
+		analyzer.use(NumberOfEdges);
+		checkSize(1);
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testUseAll() {
-		analyzer.use(ModelMetrics.NumberOfEdges);
+	@Test
+	public void testUseWithName() {
+		String name = "CustomName";
+		analyzer.use(NumberOfEdges, name);
+		assertEquals(name, analyzer.getMetric(ModelMetrics.NumberOfEdges).getName());
+	}
+
+	@Test
+	public void testMultipleDifferentUse() {
+		analyzer.use(NumberOfEdges);
+		analyzer.use(NumberOfNodes);
+		checkSize(2);
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testMultipleSameUse() {
+		analyzer.use(NumberOfEdges);
+		analyzer.use(NumberOfEdges);
+	}
+
+	@Test
+	public void testGetMetric() {
+		analyzer.use(NumberOfEdges);
+		analyzer.use(NumberOfNodes);
+		checkMetric(NumberOfEdges);
+		checkMetric(NumberOfNodes);
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testIncorrectUseAll() {
+		analyzer.use(NumberOfEdges);
 		analyzer.useAll();
 	}
+
+	@Test
+	public void testUseAll() {
+		analyzer.useAll();
+		checkSize(ModelMetrics.values().length);
+
+		checkMetric(NumberOfNodes);
+		checkMetric(NumberOfEdges);
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testIncorrectOmit() {
+		analyzer.omit(NumberOfNodes);
+	}
+
+	@Test
+	public void testOmittingAbsenceMetric() {
+		analyzer.use(NumberOfEdges).omit(NumberOfNodes);
+		checkSize(1);
+		checkMetric(NumberOfEdges);
+	}
+
+	@Test
+	public void testOmittingExistingMetric() {
+		analyzer.use(NumberOfNodes).omit(NumberOfNodes);
+		checkSize(0);
+	}
+
+	@Test
+	public void testOmitWithAll() {
+		analyzer.useAll().omit(NumberOfNodes);
+		checkSize(ModelMetrics.values().length - 1);
+		assertNull(analyzer.getMetric(NumberOfNodes));
+
+		analyzer.clearMetrics();
+		analyzer.useAll().omit(NumberOfEdges);
+
+		checkSize(ModelMetrics.values().length - 1);
+		assertNull(analyzer.getMetric(NumberOfEdges));
+
+		analyzer.clearMetrics();
+		analyzer.useAll().omit(NumberOfNodes).omit(NumberOfEdges);
+		checkSize(0);
+	}
+
+	@Test
+	public void testOrder() {
+		fail("Not tested yet");
+	}
+
 }
--- eu.mondo.map.modelanalyzer/src/eu/mondo/map/modelanalyzer/ModelAnalyzer.java
+++ eu.mondo.map.modelanalyzer/src/eu/mondo/map/modelanalyzer/ModelAnalyzer.java
@@ -11,22 +11,19 @@ import eu.mondo.map.modelmetrics.ModelMetrics;
 
 public class ModelAnalyzer extends Analyzer<String, Metric> {
 
-	// public ModelAnalyzer(final String modelName, final int sampleSize) {
-	// this.modelName = modelName;
-	// this.sampleSize = sampleSize;
-	// }
-
 	private final Logger logger = Logger.getLogger(this.getClass());
 
 	/**
 	 * <p>
 	 * Adds the parameter metric (see {@link ModelMetrics}) to the metrics
-	 * collection which will be used in the analysis.
+	 * collection which will be used in the analysis. Throws
+	 * IllegalStateException if the metric was already added to this class.
 	 * </p>
 	 * 
 	 * @param metric
 	 *            represents the metric
-	 * 
+	 * @throws IllegalStateException
+	 *             if metric was already added
 	 * @return this
 	 */
 	public ModelAnalyzer use(ModelMetrics metric) {
@@ -38,7 +35,8 @@ public class ModelAnalyzer extends Analyzer<String, Metric> {
 	 * <p>
 	 * Adds the parameter metric (see {@link ModelMetrics}) to the
 	 * <em>metrics</em> collection which will be used in the analysis. The
-	 * metric is identified by the given name parameter.
+	 * metric is identified by the given name parameter. Throws
+	 * IllegalStateException if the metric was already added to this class.
 	 * </p>
 	 * 
 	 * @param metric
@@ -46,6 +44,8 @@ public class ModelAnalyzer extends Analyzer<String, Metric> {
 	 * 
 	 * @param name
 	 *            String, represents the name of the metric
+	 * @throws IllegalStateException
+	 *             if metric was already added
 	 * 
 	 * @return this
 	 */
@@ -54,41 +54,44 @@ public class ModelAnalyzer extends Analyzer<String, Metric> {
 		return this;
 	}
 
-	/**
-	 * <p>
-	 * Adds the parameter metric (see {@link Metric}) to the <em>metrics</em>
-	 * collection which will be used in the analysis.
-	 * </p>
-	 * 
-	 * @param metric
-	 *            {@link Metric} object
-	 * @return this
-	 */
-	public ModelAnalyzer use(Metric metric) {
-		metrics.put(metric.getName(), metric);
-		return this;
-	}
+	// /**
+	// * <p>
+	// * Adds the parameter metric (see {@link Metric}) to the <em>metrics</em>
+	// * collection which will be used in the analysis.
+	// * </p>
+	// *
+	// * @param metric
+	// * {@link Metric} object
+	// * @return this
+	// */
+	// public ModelAnalyzer use(Metric metric) {
+	// metrics.put(metric.getName(), metric);
+	// return this;
+	// }
 
-	/**
-	 * <p>
-	 * Adds the parameter metric (see {@link Metric}) to the <em>metrics</em>
-	 * collection which will be used in the analysis. The metric is identified
-	 * by the given name parameter.
-	 * </p>
-	 * 
-	 * @param metric
-	 *            {@link Metric} object
-	 * @param name
-	 *            String, represents the name of the metric
-	 * @return this
-	 */
-	public ModelAnalyzer use(Metric metric, String name) {
-		metric.setName(name);
-		metrics.put(metric.getName(), metric);
-		return this;
-	}
+	// /**
+	// * <p>
+	// * Adds the parameter metric (see {@link Metric}) to the <em>metrics</em>
+	// * collection which will be used in the analysis. The metric is identified
+	// * by the given name parameter.
+	// * </p>
+	// *
+	// * @param metric
+	// * {@link Metric} object
+	// * @param name
+	// * String, represents the name of the metric
+	// *
+	// * @return this
+	// */
+	// public ModelAnalyzer use(Metric metric, String name) {
+	// metric.setName(name);
+	// metrics.put(metric.getName(), metric);
+	// return this;
+	// }
 
 	protected void useMetric(ModelMetrics metric, String name) {
+		checkState(!metrics.containsKey(metric.toString()),
+				"The metric " + metric.toString() + " was already added to the analyzer.");
 		Metric newMetric = metric.instantiate();
 		if (name != null) {
 			newMetric.setName(name);
@@ -110,7 +113,7 @@ public class ModelAnalyzer extends Analyzer<String, Metric> {
 	 * @return
 	 */
 	public ModelAnalyzer useAll() {
-		checkState(!metrics.isEmpty(),
+		checkState(metrics.isEmpty(),
 				"The ModelAnalyzer already contains metrics, so the useAll function cannot be used.");
 		for (ModelMetrics m : ModelMetrics.values()) {
 			useMetric(m, null);
@@ -141,7 +144,7 @@ public class ModelAnalyzer extends Analyzer<String, Metric> {
 	 * @return this
 	 */
 	public ModelAnalyzer omit(ModelMetrics metric) {
-		checkState(metrics.isEmpty(), "The ModelAnalyzer should contain metrics to omit " + metric.toString()
+		checkState(!metrics.isEmpty(), "The ModelAnalyzer should contain metrics to omit " + metric.toString()
 				+ " from the analysis, but it is empty.");
 		if (!metrics.containsKey(metric.toString())) {
 			logger.warn("The ModelAnalyzer does not contain " + metric.toString() + " which is planned to be omitted.");
@@ -166,233 +169,4 @@ public class ModelAnalyzer extends Analyzer<String, Metric> {
 		return metrics.get(metric.toString());
 	}
 
-	// protected final int sampleSize;
-	// protected final String modelName;
-	// protected Network<N> network;
-	//
-	// // scalar metrics
-	// protected NumberOfEdges numberOfEdges = new NumberOfEdges();
-	// protected NumberOfNodes numberOfNodes = new NumberOfNodes();
-	// protected Density density = new Density();
-	//
-	// // typed scalar metrics
-	// protected DimensionActivity dimensionActivity = new DimensionActivity();
-	// protected EdgeDimensionConnectivity edgeDimensionConnectivity = new
-	// EdgeDimensionConnectivity();
-	// protected NodeDimensionConnectivity nodeDimensionConnectivity = new
-	// NodeDimensionConnectivity();
-	// protected NodeDimensionConnectivity nodeExclusiveDimensionConnectivity =
-	// new NodeDimensionConnectivity();
-	// protected NumberOfTypedNodes numberOfTypedNodes = new
-	// NumberOfTypedNodes();
-	// protected NumberOfTypedEdges numberOfTypedEdges = new
-	// NumberOfTypedEdges();
-	// protected PairwiseMultiplexity pairwiseMultiplexity = new
-	// PairwiseMultiplexity();
-	//
-	// // composite metrics
-	// protected ClusteringCoefficientList<N> clusteringCoefficientList = new
-	// ClusteringCoefficientList<>();
-	// protected DegreeList<N> degreeList = new DegreeList<>();
-	// protected DimensionalClusteringCoefficient
-	// dimensionalClusteringCoefficientDef1 = new
-	// DimensionalClusteringCoefficient();
-	// protected DimensionalClusteringCoefficient
-	// dimensionalClusteringCoefficientDef2 = new
-	// DimensionalClusteringCoefficient();
-	// protected DimensionalClusteringCoefficient
-	// dimensionalClusteringCoefficientDef3 = new
-	// DimensionalClusteringCoefficient();
-	// protected MultiplexParticipationCoefficient
-	// multiplexParticipationCoefficient = new
-	// MultiplexParticipationCoefficient();
-	// protected NodeInterdependenceList<N> nodeInterdependenceList = new
-	// NodeInterdependenceList<>();
-	// protected ShortestPathList<N> shortestPathList = new
-	// ShortestPathList<>();
-	// protected NeighborhoodList<N> neighborhoods = new NeighborhoodList<>();
-	//
-	// // typed composite metrics
-	// protected DimensionalTypedClusteringCoefficientList<N>
-	// dimensionalTypedClusteringCoefficientList = new
-	// DimensionalTypedClusteringCoefficientList<>();
-	// protected NodeActivityList<N> nodeActivityList = new
-	// NodeActivityList<>();
-	// protected DimensionalDegreeList<N> dimensionalDegreeList = new
-	// DimensionalDegreeList<>();
-	//
-	// // init methods
-	// protected abstract void initNetwork() throws Exception;
-	//
-	// protected abstract void initModel() throws Exception;
-	//
-	// public void run() throws Exception {
-	// System.out.println("Calculating metrics for: " + modelName);
-	// initModel();
-	// initNetwork();
-	// initFields();
-	// calculateMetrics();
-	// saveMetrics();
-	// }
-	//
-	// protected void initFields() {
-	// // scalar metrics
-	// addMetric(density);
-	// addMetric(numberOfEdges);
-	// addMetric(numberOfNodes);
-	//
-	// // typed scalar metrics
-	// addMetric(dimensionActivity);
-	// addMetric(edgeDimensionConnectivity);
-	// addMetric(nodeDimensionConnectivity);
-	// addMetric(nodeExclusiveDimensionConnectivity);
-	// addMetric(numberOfTypedNodes);
-	// addMetric(numberOfTypedEdges);
-	// addMetric(pairwiseMultiplexity);
-	//
-	// // composite metrics
-	// addMetric(clusteringCoefficientList);
-	// addMetric(degreeList);
-	// addMetric(dimensionalClusteringCoefficientDef1);
-	// addMetric(dimensionalClusteringCoefficientDef2);
-	// addMetric(dimensionalClusteringCoefficientDef3);
-	// addMetric(multiplexParticipationCoefficient);
-	// addMetric(nodeInterdependenceList);
-	// addMetric(shortestPathList);
-	// addMetric(neighborhoods);
-	//
-	// // typed composite metrics
-	// addMetric(dimensionalDegreeList);
-	// addMetric(dimensionalTypedClusteringCoefficientList);
-	// addMetric(nodeActivityList);
-	//
-	// System.out.println("Initialized fields");
-	// }
-	//
-	// protected void calculateMetrics() {
-	// // scalar metrics
-	// System.out.println(numberOfNodes.getClass());
-	// numberOfNodes.calculate(network);
-	//
-	// System.out.println(numberOfEdges.getClass());
-	// numberOfEdges.calculate(network);
-	//
-	// System.out.println(density.getClass());
-	// density.calculate(numberOfNodes, numberOfEdges);
-	//
-	// // typed scalar metrics
-	//
-	// System.out.println(dimensionActivity.getClass());
-	// dimensionActivity.calculate(network);
-	//
-	// System.out.println(edgeDimensionConnectivity.getClass());
-	// edgeDimensionConnectivity.calculate(network);
-	//
-	// System.out.println(nodeDimensionConnectivity.getClass());
-	// nodeDimensionConnectivity.calculate(network);
-	//
-	// nodeExclusiveDimensionConnectivity.setName("NodeExclusiveDimensionConnectivity");
-	// System.out.println(nodeExclusiveDimensionConnectivity.getClass());
-	// nodeExclusiveDimensionConnectivity.calculate(network);
-	//
-	// System.out.println(numberOfTypedEdges.getClass());
-	// numberOfTypedEdges.calculate(network);
-	//
-	// System.out.println(pairwiseMultiplexity.getClass());
-	// pairwiseMultiplexity.calculateAllPairs(network, true);
-	//
-	// System.out.println(pairwiseMultiplexity.getClass());
-	// pairwiseMultiplexity.calculateAllPairsExclusive(network, true);
-	//
-	// // composite metrics
-	// System.out.println(clusteringCoefficientList.getClass());
-	// clusteringCoefficientList.setUseHeuristic(true);
-	// clusteringCoefficientList.calculate(network);
-	//
-	// System.out.println(degreeList.getClass());
-	// degreeList.calculate(network);
-	//
-	// System.out.println(dimensionalClusteringCoefficientDef1.getClass());
-	// dimensionalClusteringCoefficientDef1.setName("DimensionalClusteringCoefficientDef1");
-	// dimensionalClusteringCoefficientDef1.setUseHeuristic(true);
-	// dimensionalClusteringCoefficientDef1.calculateFirstDefinition(network);
-	//
-	// System.out.println(dimensionalClusteringCoefficientDef2.getClass());
-	// dimensionalClusteringCoefficientDef2.setName("DimensionalClusteringCoefficientDef2");
-	// dimensionalClusteringCoefficientDef2.setUseHeuristic(true);
-	// dimensionalClusteringCoefficientDef2.calculateSecondDefinition(network);
-	//
-	// System.out.println(dimensionalClusteringCoefficientDef3.getClass());
-	// dimensionalClusteringCoefficientDef3.setName("DimensionalClusteringCoefficientDef3");
-	// dimensionalClusteringCoefficientDef3.setUseHeuristic(true);
-	// dimensionalClusteringCoefficientDef3.calculateSecondDefinition(network);
-	//
-	// System.out.println(multiplexParticipationCoefficient.getClass());
-	// multiplexParticipationCoefficient.calculate(network);
-	//
-	// System.out.println(neighborhoods.getClass());
-	// neighborhoods.calculate(network);
-	//
-	// // if (sampleSize == 0) {
-	// // System.out.println(shortestPathList.getClass());
-	// // shortestPathList.calculate(network);
-	// //
-	// // System.out.println(nodeInterdependenceList.getClass());
-	// // nodeInterdependenceList.calculate(network);
-	// // } else {
-	// // System.out.println(shortestPathList.getClass());
-	// // shortestPathList.calculate(network, sampleSize);
-	// //
-	// // System.out.println(nodeInterdependenceList.getClass());
-	// // nodeInterdependenceList.calculate(network, sampleSize);
-	// // }
-	//
-	// // typed composite metrics
-	// System.out.println(dimensionalDegreeList.getClass());
-	// dimensionalDegreeList.calculate(network);
-	//
-	// if (sampleSize == 0) {
-	// System.out.println(dimensionalTypedClusteringCoefficientList.getClass());
-	// dimensionalTypedClusteringCoefficientList.calculate(network);
-	// } else {
-	// System.out.println(dimensionalTypedClusteringCoefficientList.getClass());
-	// dimensionalTypedClusteringCoefficientList.calculate(network, sampleSize);
-	// }
-	//
-	// System.out.println(nodeActivityList.getClass());
-	// nodeActivityList.calculate(network);
-	//
-	// System.out.println("Calculated metrics");
-	// }
-	//
-	// protected static final String NEWLINE = "\n";
-	// protected static final String DELIMITER = "\t";
-	// protected static final String HEADER = "Category" + DELIMITER +
-	// "Instance" + DELIMITER + "Index" + DELIMITER
-	// + "Value";
-	//
-	// protected void saveMetrics() throws IOException {
-	// final Object samplePostfix = sampleSize == 0 ? "" : "-" + sampleSize;
-	// try (final FileWriter fileWriter = new FileWriter(modelName +
-	// samplePostfix + getPostfix() + ".tsv");) {
-	// fileWriter.append(HEADER);
-	// fileWriter.append(NEWLINE);
-	// for (final PublishedMetric metric : resolve()) {
-	// fileWriter.append(metric.getCategory());
-	// fileWriter.append(DELIMITER);
-	// fileWriter.append(metric.getInstanceString());
-	// fileWriter.append(DELIMITER);
-	// fileWriter.append(metric.getIndexString());
-	// fileWriter.append(DELIMITER);
-	// fileWriter.append(metric.getValue().toString());
-	// fileWriter.append(NEWLINE);
-	// }
-	// fileWriter.flush();
-	// }
-	// System.out.println("Saved results");
-	// }
-	//
-	// protected String getPostfix() {
-	// return "";
-	// }
 }
