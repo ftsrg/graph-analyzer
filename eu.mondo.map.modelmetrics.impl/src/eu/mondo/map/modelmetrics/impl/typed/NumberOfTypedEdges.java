d1b98faff5fab5f69241d544bb91bd2708feeddc
ZsoltKovari <kovari.zsolt91@gmail.com>
2016-06-13 12:54:28 +0200

    NodeDimensionConnectivity, NumberOfTypedEdges

--- eu.mondo.map.modelmetrics.impl/src/eu/mondo/map/modelmetrics/impl/typed/NumberOfTypedEdges.java
+++ eu.mondo.map.modelmetrics.impl/src/eu/mondo/map/modelmetrics/impl/typed/NumberOfTypedEdges.java
@@ -1,25 +1,46 @@
 package eu.mondo.map.modelmetrics.impl.typed;
 
-import eu.mondo.map.core.graph.Network;
-import eu.mondo.map.core.graph.Node;
-import eu.mondo.map.core.metrics.typed.TypedScalarMetric;
+import static eu.mondo.map.modelmetrics.impl.typed.TypedModelMetric.castAdapter;
 
-public class NumberOfTypedEdges extends TypedScalarMetric<String, Integer> {
+import eu.mondo.map.core.metrics.typed.TypedScalarMetric;
+import eu.mondo.map.modeladapters.ModelAdapter;
+import eu.mondo.map.modeladapters.TypedModelAdapter;
+import eu.mondo.map.modelmetrics.ModelEvaluator;
+
+public class NumberOfTypedEdges extends TypedScalarMetric<String, Integer> implements ModelEvaluator {
 
 	public NumberOfTypedEdges() {
 		super("NumberOfTypedEdges");
 	}
 
-	public void calculate(final Network<?> network) {
-		clear();
-		for (String dimension : network.getNodesOnDimensions().keySet()) {
+	// public void calculate(final Network<?> network) {
+	// clear();
+	// for (String dimension : network.getNodesOnDimensions().keySet()) {
+	// int sumOfEdges = 0;
+	// for (Node<?> node : network.getNodesOnDimensions().get(dimension)) {
+	// sumOfEdges += node.getNumberOfNeighbors(dimension);
+	// }
+	// sumOfEdges /= 2;
+	// typedValues.put(dimension, sumOfEdges);
+	// }
+	// }
+
+	@Override
+	public <M> void evaluate(ModelAdapter<M> adapter) {
+		TypedModelAdapter<M> typedAdapter = castAdapter(adapter);
+		for (Object type : typedAdapter.getTypes()) {
 			int sumOfEdges = 0;
-			for (Node<?> node : network.getNodesOnDimensions().get(dimension)) {
-				sumOfEdges += node.getNumberOfNeighbors(dimension);
+			for (Object node : typedAdapter.getNodes(type)) {
+				sumOfEdges += typedAdapter.getDegree(node, type);
 			}
 			sumOfEdges /= 2;
-			typedValues.put(dimension, sumOfEdges);
+			typedValues.put(type.toString(), sumOfEdges);
 		}
+	}
+
+	@Override
+	public <M> void evaluate(ModelAdapter<M> adapter, Object element) {
+		throw new UnsupportedOperationException("Cannot evaluate NumberOfTypedEdges metric on an element.");
 	}
 
 }
