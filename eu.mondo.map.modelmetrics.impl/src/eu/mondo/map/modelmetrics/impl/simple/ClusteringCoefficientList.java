5fd19b0d0fe46afa1256f87be165f73959178480
ZsoltKovari <kovari.zsolt91@gmail.com>
2016-06-13 10:34:53 +0200

    Change clustering coef evaluation based on model adapters

--- eu.mondo.map.modelmetrics.impl/src/eu/mondo/map/modelmetrics/impl/simple/ClusteringCoefficientList.java
+++ eu.mondo.map.modelmetrics.impl/src/eu/mondo/map/modelmetrics/impl/simple/ClusteringCoefficientList.java
@@ -2,8 +2,6 @@ package eu.mondo.map.modelmetrics.impl.simple;
 
 import java.util.Iterator;
 
-import eu.mondo.map.core.graph.Network;
-import eu.mondo.map.core.graph.Node;
 import eu.mondo.map.core.metrics.ListMetric;
 import eu.mondo.map.modeladapters.ModelAdapter;
 import eu.mondo.map.modelmetrics.ModelEvaluator;
@@ -33,46 +31,48 @@ public class ClusteringCoefficientList<N> extends ListMetric<Double> implements 
 		this.useHeuristic = useHeuristic;
 	}
 
-	public void calculate(final Network<N> network) {
-		clear();
-		int i = 0;
-		for (Node<N> node : network.getNodes()) {
-			calculate(node);
-			i++;
-			if (i % 100000 == 0) {
-				System.out.println(i);
-			}
-		}
-	}
-
-	public double calculate(final Node<N> node) {
-		long interConnected = 0;
-		long numberOfNeighbors = 0;
-		double clusteringCoef = 0.0;
-		if (useHeuristic && node.getNumberOfDisjunctNeighbors() > maxNeighbours) {
-			values.add(clusteringCoef);
-			return clusteringCoef;
-		}
-		for (Node<N> neighbor1 : node.getDisjunctNeighbors()) {
-			for (Node<N> neighbor2 : node.getDisjunctNeighbors()) {
-				if (neighbor1 != neighbor2) {
-					if (neighbor1.hasNeighbor(neighbor2)) {
-						interConnected++;
-					}
-				}
-			}
-		}
-
-		numberOfNeighbors = node.getNumberOfDisjunctNeighbors();
-		if (numberOfNeighbors < 2) {
-			clusteringCoef = 0.0;
-		} else {
-			clusteringCoef = interConnected / (double) (numberOfNeighbors * (numberOfNeighbors - 1));
-
-		}
-		values.add(clusteringCoef);
-		return clusteringCoef;
-	}
+	// public void calculate(final Network<N> network) {
+	// clear();
+	// int i = 0;
+	// for (Node<N> node : network.getNodes()) {
+	// calculate(node);
+	// i++;
+	// if (i % 100000 == 0) {
+	// System.out.println(i);
+	// }
+	// }
+	// }
+	//
+	// public double calculate(final Node<N> node) {
+	// long interConnected = 0;
+	// long numberOfNeighbors = 0;
+	// double clusteringCoef = 0.0;
+	// if (useHeuristic && node.getNumberOfDisjunctNeighbors() > maxNeighbours)
+	// {
+	// values.add(clusteringCoef);
+	// return clusteringCoef;
+	// }
+	// for (Node<N> neighbor1 : node.getDisjunctNeighbors()) {
+	// for (Node<N> neighbor2 : node.getDisjunctNeighbors()) {
+	// if (neighbor1 != neighbor2) {
+	// if (neighbor1.hasNeighbor(neighbor2)) {
+	// interConnected++;
+	// }
+	// }
+	// }
+	// }
+	//
+	// numberOfNeighbors = node.getNumberOfDisjunctNeighbors();
+	// if (numberOfNeighbors < 2) {
+	// clusteringCoef = 0.0;
+	// } else {
+	// clusteringCoef = interConnected / (double) (numberOfNeighbors *
+	// (numberOfNeighbors - 1));
+	//
+	// }
+	// values.add(clusteringCoef);
+	// return clusteringCoef;
+	// }
 
 	@Override
 	public <M> void evaluate(ModelAdapter<M> adapter) {
@@ -87,21 +87,21 @@ public class ClusteringCoefficientList<N> extends ListMetric<Double> implements 
 		long interConnected = 0;
 		long numberOfNeighbors = 0;
 		double clusteringCoef = 0.0;
-		if (useHeuristic && node.getNumberOfDisjunctNeighbors() > maxNeighbours) {
+		if (useHeuristic && adapter.getDegree(element) > maxNeighbours) {
 			values.add(clusteringCoef);
 			// return clusteringCoef;
 		}
-		for (Node<N> neighbor1 : node.getDisjunctNeighbors()) {
-			for (Node<N> neighbor2 : node.getDisjunctNeighbors()) {
+		for (Object neighbor1 : adapter.getNeighbors(element)) {
+			for (Object neighbor2 : adapter.getNeighbors(element)) {
 				if (neighbor1 != neighbor2) {
-					if (neighbor1.hasNeighbor(neighbor2)) {
+					if (adapter.isAdjacent(neighbor1, neighbor2)) {
 						interConnected++;
 					}
 				}
 			}
 		}
 
-		numberOfNeighbors = node.getNumberOfDisjunctNeighbors();
+		numberOfNeighbors = adapter.getDegree(element);
 		if (numberOfNeighbors < 2) {
 			clusteringCoef = 0.0;
 		} else {
