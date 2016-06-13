ec2df62109e9a30a3ec3e4a3ab51f4145834c749
ZsoltKovari <kovari.zsolt91@gmail.com>
2016-06-13 12:41:41 +0200

    Modify NodeActivity using model adapter, delete AbstractModelMetric

--- eu.mondo.map.modelmetrics.impl/src/eu/mondo/map/modelmetrics/impl/typed/NodeActivityList.java
+++ eu.mondo.map.modelmetrics.impl/src/eu/mondo/map/modelmetrics/impl/typed/NodeActivityList.java
@@ -1,20 +1,38 @@
 package eu.mondo.map.modelmetrics.impl.typed;
 
-import eu.mondo.map.core.graph.Network;
-import eu.mondo.map.core.graph.Node;
-import eu.mondo.map.core.metrics.ListMetric;
+import static eu.mondo.map.modelmetrics.impl.typed.TypedModelMetric.castAdapter;
+import static eu.mondo.map.modelmetrics.impl.typed.TypedModelMetric.evaluateEveryNode;
 
-public class NodeActivityList<N> extends ListMetric<Integer> {
+import eu.mondo.map.core.metrics.ListMetric;
+import eu.mondo.map.modeladapters.ModelAdapter;
+import eu.mondo.map.modeladapters.TypedModelAdapter;
+import eu.mondo.map.modelmetrics.ModelEvaluator;
+
+public class NodeActivityList<N> extends ListMetric<Integer> implements ModelEvaluator {
 
 	public NodeActivityList() {
 		super("NodeActivityList");
 	}
 
-	public void calculate(final Network<N> network) {
-		clear();
-		for (Node<N> node : network.getNodes()) {
-			values.add(node.getDimensions().keySet().size());
-		}
+	// public void calculate(final Network<N> network) {
+	// clear();
+	// for (Node<N> node : network.getNodes()) {
+	// values.add(node.getDimensions().keySet().size());
+	// }
+	// }
+
+	@Override
+	public <M> void evaluate(ModelAdapter<M> adapter) {
+		evaluateEveryNode(adapter, this);
+	}
+
+	@Override
+	public <M> void evaluate(ModelAdapter<M> adapter, Object element) {
+		evaluate(castAdapter(adapter), element);
+	}
+
+	protected <M> void evaluate(TypedModelAdapter<M> adapter, Object element) {
+		values.add(adapter.getNumberOfTypes(element));
 	}
 
 }
