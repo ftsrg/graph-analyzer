5e3eb8bdb3de374677de01249d0bee641c10b2d3
Gábor Szárnyas <szarnyasg@gmail.com>
2018-05-31 10:50:02 +0200

    Prettify filename

--- reporting/R/visualization/MetricVisualizationGeneral.R
+++ reporting/R/visualization/MetricVisualizationGeneral.R
@@ -10,21 +10,13 @@ source("R/transformation/ImportTSVs.R")
 figures.path <- "reports/"
 default.theme <- theme_bw() + theme(legend.position = "bottom")
 
+# put the tsv files to the tsv directory
 tsvs <- list.files("tsv", pattern = ".tsv", full.names = T, recursive = T)
-
-# uncomment this for testing
-# tsvs <- c(
-#   "../models-metrics/VW_1.xmi-nd.tsv", 
-#   "../models-metrics/VW_2.xmi-nd.tsv", 
-#   "../models-metrics/VW_1.xmi-nd.tsv", 
-#   "../models-metrics/R_201631.xmi-nd.tsv", 
-#   "../models-metrics/R_2017156.xmi-nd.tsv",
-#   "../models-metrics/R_2017254.xmi-nd.tsv"
-#   )
 
 l <- lapply(tsvs, ImportsTSVToDataTable, cont = F)
 tsv <- rbindlist(l)
-tsv$file.name = gsub("\\.\\./models-metrics/", "", tsv$file.name)
+tsv$file.name = gsub("\\.tsv", "", tsv$file.name)
+tsv$file.name = gsub("tsv/",   "", tsv$file.name)
 #tsv[, file.name := as.numeric(as.factor(file.name))]
 #tsv[, file.name := paste0("File ", file.name)]
 #tsv[, file.name := factor(file.name, levels = paste0("File ", seq_len(length(unique(tsv$file.name)))))]
@@ -61,8 +53,9 @@ DimensionalEdgeVisualization(edge.type, basic.information, figures.path)
 
 ############# General node metrics ######################
 node.type <- subset(tsv, Category %in% c(#"ClusteringCoefficientList",
-  "DegreeList",
-  "MultiplexParticipationCoefficient"
+#  "DegreeList",
+  "MultiplexParticipationCoefficient",
+  "DimensionalClusteringCoefficient"
 ))
 node.type <- dcast.data.table(data = node.type, file.name + category + Index ~ Category, value.var = "Value")
 GeneralNodeVisualization(node.type, figures.path)
@@ -71,6 +64,9 @@ GeneralNodeVisualization(node.type, figures.path)
 #### Pairwise multiplexity ##############################
 pairwise <- subset(tsv, Category %in% "PairwiseMultiplexity")
 setnames(pairwise, "Value", "PairwiseMultiplexity")
+PlotsECDFByFileNameOneSide(pairwise, paste0(figures.path, "PairwiseMultiplexity_ecdf_oneside.pdf"), x = "PairwiseMultiplexity", col = "category", title = 'PairwiseMultiplexity')
 
-PlotsECDFByFileName(pairwise, paste0(figures.path, "PairwiseMultiplexity_ecdf_faceted.pdf"), x = "PairwiseMultiplexity", col = "category", title = 'PairwiseMultiplexity')
-#PlotsECDFByFileNameOneSide(pairwise, paste0(figures.path, "PairwiseMultiplexity_ecdf_oneside.pdf"), x = "PairwiseMultiplexity", col = "category", title = 'PairwiseMultiplexity')
+#### Edge overlap ##############################
+edgeoverlap <- subset(tsv, Category %in% "EdgeOverlap")
+setnames(edgeoverlap, "Value", "EdgeOverlap")
+PlotsECDFByFileNameOneSide(pairwise, paste0(figures.path, "EdgeOverlap_ecdf_oneside.pdf"), x = "PairwiseMultiplexity", col = "category", title = 'PairwiseMultiplexity')
