46897407b6dcc621036136667fce6da7d5fef0d7
Gabor Szarnyas <szarnyasg@gmail.com>
2018-10-05 14:19:00 +0200

    Add scale factor to sampling script

--- analyzer-app/graphs/sample.sh
+++ analyzer-app/graphs/sample.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# usage: ./sample.sh <graph-name> <number-of-edges>
+# usage: ./sample.sh <graph-name> <number-of-edges> <SF>
 
-cat $1-full/$1-edges.csv | head -n $2 > $1-edges.csv
+cat $1-full/$1-edges.csv | head -n $2 > $1-edges-$3.csv
 cat $1-edges.csv | cut -f 1 > source-nodes.csv
 cat $1-edges.csv | cut -f 3 > target-nodes.csv
-cat source-nodes.csv target-nodes.csv | sort -u > $1-nodes.csv
+cat source-nodes.csv target-nodes.csv | sort -u > $1-nodes-$3.csv
 
 wc -l $1-nodes.csv
 wc -l $1-edges.csv
