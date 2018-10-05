56aabf17617537e109969e82f550acc835448f03
Gabor Szarnyas <szarnyasg@gmail.com>
2018-10-05 14:35:13 +0200

    Swap SF and nodes|edges in sampled CSV names

--- analyzer-app/graphs/sample.sh
+++ analyzer-app/graphs/sample.sh
@@ -2,11 +2,11 @@
 
 # usage: ./sample.sh <graph-name> <number-of-edges> <SF>
 
-cat $1-full/$1-edges.csv | head -n $2 > $1-edges-$3.csv
-cat $1-edges-$3.csv | cut -f 1 > source-nodes.csv
-cat $1-edges-$3.csv | cut -f 3 > target-nodes.csv
-cat source-nodes.csv target-nodes.csv | sort -u > $1-nodes-$3.csv
+cat $1-full/$1-edges.csv | head -n $2 > $1-$3-edges.csv
+cat $1-$3-edges.csv | cut -f 1 > source-nodes.csv
+cat $1-$3-edges.csv | cut -f 3 > target-nodes.csv
+cat source-nodes.csv target-nodes.csv | sort -u > $1-$3-nodes.csv
 rm source-nodes.csv target-nodes.csv
 
-wc -l $1-nodes-$3.csv
-wc -l $1-edges-$3.csv
+wc -l $1-$3-nodes.csv
+wc -l $1-$3-edges.csv
