1fbdeb129eebb682b369b1d8d449c65405f61d90
Gabor Szarnyas <szarnyasg@gmail.com>
2018-10-07 14:57:08 +0200

    mxmult

--- julia/local-clustering-coefficient.jl
+++ julia/local-clustering-coefficient.jl
@@ -5,16 +5,11 @@
 # Example: julia graph-to-parse.jl test-vertices.csv test-edges.csv
 ###############################################################################
 
-NODE_FILE_PATH = "test-nodes.csv"
-EDGE_FILE_PATH = "test-edges.csv"
+EDGE_FILE1_PATH = "f1.txt"
+EDGE_FILE2_PATH = "f2.txt"
 
 # Count the number of vertices
-vertex_counter = 0
-open(NODE_FILE_PATH) do f
-    for l in eachline(f)
-        global vertex_counter += 1
-    end
-end
+vertex_counter = 626892
 
 # Triplets to store edges
 I = Int64[]
@@ -23,25 +18,49 @@ V = Int64[]
 
 # Loop through the edges, and fill the triplets with (i, j, 1) format.
 # Increase the nodes id by 1 to match Julias array indices.
-open(EDGE_FILE_PATH) do f
+open(EDGE_FILE1_PATH) do f
     for l in eachline(f)
         splitted = split(l, "\t")
         vertex1 = parse(Int64, splitted[1]) + 1
-        vertex2 = parse(Int64, splitted[3]) + 1
+        vertex2 = parse(Int64, splitted[2]) + 1
+
         append!(I, [vertex1])
         append!(J, [vertex2])
+        append!(V, [1])
+
+        append!(I, [vertex2])
+        append!(J, [vertex1])
         append!(V, [1])
     end
 end
 
+S1 = sparse(I, J, V, vertex_counter, vertex_counter)
+
+I = Int64[]
+J = Int64[]
+V = Int64[]
+open(EDGE_FILE2_PATH) do f
+    for l in eachline(f)
+        splitted = split(l, "\t")
+        vertex1 = parse(Int64, splitted[1]) + 1
+        vertex2 = parse(Int64, splitted[2]) + 1
+
+        append!(I, [vertex1])
+        append!(J, [vertex2])
+        append!(V, [1])
+
+        append!(I, [vertex2])
+        append!(J, [vertex1])
+        append!(V, [1])
+    end
+end
+S2 = sparse(I, J, V, vertex_counter, vertex_counter)
+
 using SparseArrays
 
-S = sparse(I, J, V, vertex_counter, vertex_counter)
 
-println(S)
-println()
 ############################ IMPLEMENT OPERATIONS ############################
 
 # Square
-println("S * S")
-println(S * S)
+println("S1 * S1")
+println(S1 * S2)
